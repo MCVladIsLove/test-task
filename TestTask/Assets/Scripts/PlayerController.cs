b8cd6cdc22fae3452cbc5371cc6536c83fed6287
MCVladIsLove <manukyan228008@mail.ru>
2023-10-20 01:55:00 +0300

    small refactoring

--- TestTask/Assets/Scripts/PlayerController.cs
+++ TestTask/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private FloatingJoystick _joystick;
     [SerializeField] private float _moveSpeed;
 
+    private Quaternion _leftSideRotation;
+    private Quaternion _rightSideRotation;
+
+    private void Awake()
+    {
+        _leftSideRotation = new Quaternion(0, 180, 0, 0);
+        _rightSideRotation = new Quaternion(0, 0, 0, 0);
+    }
     void Update()
     {
         transform.position = transform.position + (Vector3)_joystick.Direction * _moveSpeed * Time.deltaTime;
-        if (_joystick.Direction.x < 0) transform.rotation = new Quaternion(0, 180, 0, 0);
-        else if (_joystick.Direction.x > 0) transform.rotation = new Quaternion(0, 0, 0, 0);
+        if (_joystick.Direction.x < 0) transform.rotation = _leftSideRotation;
+        else if (_joystick.Direction.x > 0) transform.rotation = _rightSideRotation;
     }
 
 }
